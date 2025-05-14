2213853f455f91d4144cbaec0999fb6b7e59103c
Kartik <kartik200421@gmail.com>
2025-05-14 15:56:52 +0530

    initial project

---
+++ backend/src/mongo/schema.db.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.model = void 0;
+const mongoose_1 = __importDefault(require("mongoose"));
+const userSchema = new mongoose_1.default.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        requied: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+});
+exports.model = mongoose_1.default.model("userSchema", userSchema);
