71c781430b6dc382c4f093f3481fd40981511721
Kartik <kartik200421@gmail.com>
2025-05-14 21:37:21 +0530

    modified some shit

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
 import dotenv from "dotenv";
 import { users } from "./src/mongo/schema.db.js";
 import { middleware } from "./src/middleware/middleware.js";
@@ -10,7 +11,6 @@ dotenv.config()
 const app = express();
 mongoose.connect(process.env.MONGO_URL);
 app.use(express.json());
-app.use(middleware)
 
 const zodUser = z.object({
   email: z.string().email(),
@@ -46,20 +46,22 @@ app.post("/api/v1/signin", async (req, res) => {
     if (!isEmailFound) {
       return res.json({ message: "Account Not Found", status: 411 });
     }
+    const token = jwt.sign({ id: isEmailFound._id }, JWT_SECRET);
+    res.json({ message: "Logged in", token: token, status: 200 });
 
-    res.json({ message: "Logged in", status: 200 });
-
-  } catch (error) { }
+  } catch (error) {
+    return res.status(411).json({ error: error.message });
+  }
 });
 
-app.post("/api/v1/content", async (req, res) => {
+app.post("/api/v1/content", middleware, async (req, res) => {
   try {
 
     // let { title, links, tags }  
   } catch (error) { }
 });
 
-app.get("/api/v1/content", async (req, res) => {
+app.get("/api/v1/content", middleware, async (req, res) => {
   try {
   } catch (error) { }
 });
