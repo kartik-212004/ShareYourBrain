31e08ed8ccf32188d9af1ca4d4c4b821cd979d70
Kartik <kartik200421@gmail.com>
2025-05-15 20:00:59 +0530

    70 percent app done

--- frontend/src/components/home.tsx
+++ frontend/src/components/home.tsx
@@ -1,14 +1,65 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Sidebar from "./Sidebar";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import { useAuth } from "@/hooks/use-auth";
 import { Button } from "./ui/button";
+import axios from "axios";
+const apiUrl = import.meta.env.VITE_API_URL;
+
+interface TokenPayload {
+  id: string;
+}
+
 export default function Home() {
   const navigate = useNavigate();
+  const { isAuthenticated, token } = useAuth();
+  const [content, setContent] = useState(null);
+  const [error, setError] = useState("");
+
   useEffect(() => {
-    if (!localStorage.getItem("token")) {
+    if (!isAuthenticated) {
       navigate("/signin");
     }
-  }, []);
+  }, [isAuthenticated, navigate]);
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      if (isAuthenticated) {
+        try {
+          const token = localStorage.getItem("token");
+          if (token) {
+            const decodedToken = jwtDecode<TokenPayload>(token);
+
+            const userId = decodedToken.id;
+            if (userId) {
+              await getContent(userId);
+            } else {
+              console.error("No user ID found in token");
+            }
+          }
+        } catch (err) {
+          console.error("Error decoding token:", err);
+        }
+      }
+    };
+
+    fetchContent();
+  }, [isAuthenticated, token]);
+
+  async function getContent(userId: string) {
+    try {
+      const tokens = localStorage.getItem("token");
+      const response = await axios.get(`${apiUrl}/api/v1/content/${userId}`, {
+        headers: { Authorization: `Bearer ${tokens}` },
+      });
+      setContent(response.data);
+    } catch (err) {
+      console.error("Error fetching content:", err);
+      setError("Error loading content");
+    }
+  }
+  console.log(content);
   return (
     <>
       <div className="flex">
@@ -24,6 +75,9 @@ export default function Home() {
                 Add Content
               </Button>
             </div>
+          </div>
+          <div className="mt-10">
+            {error ? <p className="text-red-500">{error}</p> : <div>hello</div>}
           </div>
         </main>
       </div>
--- frontend/src/hooks/use-auth.ts
+++ frontend/src/hooks/use-auth.ts
@@ -1,16 +1,22 @@
 import { useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
 
 export function useAuth() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(() => {
+    return !!localStorage.getItem("token");
+  });
+  const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       setIsAuthenticated(true);
+      setToken(jwtDecode(token));
     } else {
       setIsAuthenticated(false);
+      setToken(null);
     }
   }, []);
 
-  return { isAuthenticated };
+  return { isAuthenticated, token };
 }
